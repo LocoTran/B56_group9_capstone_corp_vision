c0ad66c7090969926a4a671338f465e19a7db36f
Loco Tran <phuloc2712@gmail.com>
2023-07-01 22:32:06 +0700

    fix bug and responsive

--- JS/script.js
+++ JS/script.js
@@ -18,31 +18,31 @@ document.getElementById("switchButton").onclick = function () {
 };
 
 // Button search open close animation
-const zoomButton = document.querySelector(".fa-search");
-const outButton = document.querySelector(".fa-close");
+const ZOOM_BTN = document.querySelector(".fa-search");
+const OUT_BTN = document.querySelector(".fa-close");
 
-const fullsearch = document.querySelector(".full_search");
+const DIV_FULL_SEARCH = document.querySelector(".full_search");
 
-zoomButton.addEventListener("click", () => {
-  zoomButton.classList.add("zoomed");
-  zoomButton.classList.remove("normal");
+ZOOM_BTN.addEventListener("click", () => {
+  ZOOM_BTN.classList.add("zoomed");
+  ZOOM_BTN.classList.remove("normal");
   // Full search open close
-  fullsearch.style.display = "block";
+  DIV_FULL_SEARCH.style.display = "block";
 });
 
-outButton.addEventListener("click", () => {
+OUT_BTN.addEventListener("click", () => {
   // Full search open close
-  fullsearch.style.display = "none";
-  if (zoomButton.classList.contains("zoomed")) {
-    zoomButton.classList.remove("zoomed");
-    zoomButton.classList.add("normal");
+  DIV_FULL_SEARCH.style.display = "none";
+  if (ZOOM_BTN.classList.contains("zoomed")) {
+    ZOOM_BTN.classList.remove("zoomed");
+    ZOOM_BTN.classList.add("normal");
   }
 });
 
 // Set position cụm nút search và đổi theme khi màn hình nhỏ
-const btn2 = document.querySelector(".button_right");
-const fheader = document.querySelector("#header");
-const btnshow = document.querySelector("#button_show");
+const BTN_RIGHT = document.querySelector(".button_right");
+const HEAD = document.querySelector("#header");
+const BTN_TOGGER = document.querySelector("#button_show");
 
 var clickCount = 0;
 document
@@ -51,23 +51,23 @@ document
     // ngăn người dùng cuộn chuột hay thao tác khác trước khi thoát khỏi phần show
     document.body.style.overflow = "hidden";
 
-    if (clickCount === 0) {
+    if (clickCount == 0) {
       // dịch chuyển 2 nút search và đổi theme
-      btn2.style.right = "15%";
+      BTN_RIGHT.style.right = "15%";
       // kéo dài header để khoảng full dc phần show
-      fheader.style.height = "250px";
-      fheader.style.border = "none";
-      btnshow.classList.add("fa-xmark");
-      btnshow.classList.remove("fa-bars");
+      HEAD.style.height = "250px";
+      HEAD.style.border = "none";
+      BTN_TOGGER.classList.add("fa-xmark");
+      BTN_TOGGER.classList.remove("fa-bars");
 
       clickCount = 1;
-    } else if (clickCount === 1) {
-      btn2.style.right = "";
-      fheader.style.height = "";
+    } else if (clickCount == 1) {
+      BTN_RIGHT.style.right = "";
+      HEAD.style.height = "";
       document.body.style.overflow = "";
-      fheader.style.border = "";
-      btnshow.classList.remove("fa-xmark");
-      btnshow.classList.add("fa-bars");
+      HEAD.style.border = "";
+      BTN_TOGGER.classList.remove("fa-xmark");
+      BTN_TOGGER.classList.add("fa-bars");
 
       clickCount = 0;
     }
@@ -76,28 +76,23 @@ document
 // Nhấn vào phần Page
 var clickPage = 0;
 document.querySelector(".page").addEventListener("click", function () {
-  if (clickPage === 0 && window.innerWidth < 992) {
+  if (clickPage == 0) {
     // kéo dài header để khoảng full dc phần show
-    fheader.style.height = "300px";
+    HEAD.style.height = "300px";
     clickPage = 1;
-  } else if (clickPage === 1) {
-    fheader.style.height = "250px";
+  } else if (clickPage == 1) {
+    HEAD.style.height = "250px";
     clickPage = 0;
   }
 });
 
 // Chỉnh màu header khi scoll xuống
-const page_active = document.getElementById(".active");
 
 window.addEventListener("scroll", function () {
   // Kiểm tra xem trang đã được cuộn xuống hay chưa
   if (window.scrollY > 0) {
-    fheader.classList.add("scrolled");
-    page_active.classList.add("scrolled");
+    HEAD.classList.add("scrolled");
   } else {
-    fheader.classList.remove("scrolled");
-    page_active.classList.remove("scrolled");
+    HEAD.classList.remove("scrolled");
   }
 });
-
-
