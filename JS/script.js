71df8860f0b82ea194ac309ed3e311760ae11df8
VinhHung2405 <hungle14431@gmail.com>
2023-07-02 22:48:40 +0700

    create gallery

--- JS/script.js
+++ JS/script.js
@@ -18,33 +18,31 @@ document.getElementById("switchButton").onclick = function () {
 };
 
 // Button search open close animation
-const zoomButton = document.querySelector(".fa-search");
-const outButton = document.querySelector(".fa-close");
+const ZOOM_BTN = document.querySelector(".fa-search");
+const OUT_BTN = document.querySelector(".fa-close");
 
-zoomButton.addEventListener("click", () => {
-  zoomButton.classList.add("zoomed");
-  zoomButton.classList.remove("normal");
+const DIV_FULL_SEARCH = document.querySelector(".full_search");
+
+ZOOM_BTN.addEventListener("click", () => {
+  ZOOM_BTN.classList.add("zoomed");
+  ZOOM_BTN.classList.remove("normal");
+  // Full search open close
+  DIV_FULL_SEARCH.style.display = "block";
 });
 
-outButton.addEventListener("click", () => {
-  if (zoomButton.classList.contains("zoomed")) {
-    zoomButton.classList.remove("zoomed");
-    zoomButton.classList.add("normal");
+OUT_BTN.addEventListener("click", () => {
+  // Full search open close
+  DIV_FULL_SEARCH.style.display = "none";
+  if (ZOOM_BTN.classList.contains("zoomed")) {
+    ZOOM_BTN.classList.remove("zoomed");
+    ZOOM_BTN.classList.add("normal");
   }
 });
 
-// Full search open close
-document
-  .querySelector(".icon-material-search")
-  .addEventListener("click", function () {
-    document.querySelector(".full_search").style.display = "block";
-  });
-
-document.querySelector(".icon-close").addEventListener("click", function () {
-  document.querySelector(".full_search").style.display = "none";
-});
-
 // Set position cụm nút search và đổi theme khi màn hình nhỏ
+const BTN_RIGHT = document.querySelector(".button_right");
+const HEAD = document.querySelector("#header");
+const BTN_TOGGER = document.querySelector("#button_show");
 
 var clickCount = 0;
 document
@@ -53,59 +51,48 @@ document
     // ngăn người dùng cuộn chuột hay thao tác khác trước khi thoát khỏi phần show
     document.body.style.overflow = "hidden";
 
-    if (clickCount === 0) {
+    if (clickCount == 0) {
       // dịch chuyển 2 nút search và đổi theme
-      document.querySelector(".button_right").style.right = "15%";
+      BTN_RIGHT.style.right = "15%";
       // kéo dài header để khoảng full dc phần show
-      document.querySelector("#header").style.height = "250px";
-      document.querySelector("#header").style.border = "none";
-      document.querySelector("#button_show").classList.add("fa-xmark");
-      document.querySelector("#button_show").classList.remove("fa-bars");
+      HEAD.style.height = "250px";
+      HEAD.style.border = "none";
+      BTN_TOGGER.classList.add("fa-xmark");
+      BTN_TOGGER.classList.remove("fa-bars");
+
       clickCount = 1;
-    } else if (clickCount === 1) {
-      document.querySelector(".button_right").style.right = "";
-      document.querySelector("#header").style.height = "";
+    } else if (clickCount == 1) {
+      BTN_RIGHT.style.right = "";
+      HEAD.style.height = "";
       document.body.style.overflow = "";
-      document.querySelector("#header").style.border = "";
-      document.querySelector("#button_show").classList.remove("fa-xmark");
-      document.querySelector("#button_show").classList.add("fa-bars");
-      
+      HEAD.style.border = "";
+      BTN_TOGGER.classList.remove("fa-xmark");
+      BTN_TOGGER.classList.add("fa-bars");
+
       clickCount = 0;
     }
   });
 
-  var clickPage = 0;
-  document
-    .querySelector(".page")
-    .addEventListener("click", function () {
-      
-      if (clickPage === 0 && window.innerWidth < 992) {
-        // dịch chuyển 2 nút search và đổi theme
-        // document.querySelector(".dropdown-menu").style.posision = "fixed";
-        // kéo dài header để khoảng full dc phần show
-        document.querySelector("#header").style.height = "300px";
-        
-        clickPage = 1;
-      } else if (clickPage === 1) {
-        // document.querySelector(".dropdown-menu").style.posision = "";
-        document.querySelector("#header").style.height = "250px";
-        
-        
-        clickPage = 0;
-      }
-    });
-
+// Nhấn vào phần Page
+var clickPage = 0;
+document.querySelector(".page").addEventListener("click", function () {
+  if (clickPage == 0) {
+    // kéo dài header để khoảng full dc phần show
+    HEAD.style.height = "300px";
+    clickPage = 1;
+  } else if (clickPage == 1) {
+    HEAD.style.height = "250px";
+    clickPage = 0;
+  }
+});
 
 // Chỉnh màu header khi scoll xuống
 
 window.addEventListener("scroll", function () {
   // Kiểm tra xem trang đã được cuộn xuống hay chưa
   if (window.scrollY > 0) {
-    document.getElementById("header").classList.add("scrolled");
-    document.getElementById(".active").classList.add("scrolled");
+    HEAD.classList.add("scrolled");
   } else {
-    // Xóa lớp "scrolled" khỏi phần tử body
-    document.getElementById("header").classList.remove("scrolled");
-    document.getElementById(".active").classList.remove("scrolled");
+    HEAD.classList.remove("scrolled");
   }
 });
